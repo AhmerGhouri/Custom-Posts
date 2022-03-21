12c3a59b368395c3eabea5db129c65560fdd92f3
AhmerGhouri <60689523+AhmerGhouri@users.noreply.github.com>
2022-03-21 18:44:25 +0500

    Custom POst
    Custom Posts with dummy data

---
+++ News & Events.js
@@ -0,0 +1,683 @@
+// var tabs = [
+
+
+//     {
+
+//         id: 1,
+//         Desc: "Convocation",
+//         URL: "https://sohailuniversity.edu.pk/",
+//         Cat: "1",
+
+//     },
+
+//     {
+
+//         id: 2,
+//         Desc: "Alumni",
+//         URL: "https://jmc.edu.pk/",
+//         Cat: "1",
+
+//     },
+
+//     {
+
+//         id: 3,
+//         Desc: "World Kidney day",
+//         URL: "https://sohailtrusthospital.org.pk/",
+//         Cat: "1",
+
+//     },
+
+//     {
+
+//         id: 4,
+//         Desc: "World Leprosy Day",
+//         URL: "https://medicarehospital.pk/",
+//         Cat: "2",
+
+//     },
+
+//     {
+
+//         id: 5,
+//         Desc: "world Health day",
+//         URL: "https://jsmu.edu.pk/",
+//         Cat: "2",
+
+//     },
+
+//     {
+
+//         id: 6,
+//         Desc: "World Labour day",
+//         URL: "https://sohailuniversity.edu.pk/",
+//         Cat: "2",
+
+//     }
+
+
+
+
+
+// ]
+
+
+
+
+let cars = [
+    {
+      "color": "purple",
+      "type": "minivan",
+      "registration": new Date('2017-01-03'),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "station wagon",
+      "registration": new Date(),
+      "capacity": 5
+    },
+    {
+      "color": "red",
+      "type": "toyota",
+      "registration": new Date(),
+      "capacity": 6
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "vigo",
+      "registration": new Date(),
+      "capacity": 7
+    },
+    {
+        "color": "red",
+        "type": "honda",
+        "registration": new Date(),
+        "capacity": 8
+    },
+
+    {
+
+      "color": "purple",
+      "type": "minivan",
+      "registration": new Date('2017-01-03'),
+      "capacity": 7
+    }
+    
+  ]
+  
+
+
+
+// let redcars = cars.filter(car => car.color === "red");
+
+var redcars = [];
+var purplecars = [];
+
+
+let carcolors = cars.filter(car => {
+
+    return car
+
+})
+
+
+
+
+
+function active(evt){
+
+
+
+  var header = document.getElementById("main")
+  var list = header.getElementsByClassName("list")
+
+
+
+  for(i = 0 ; i < list.length ; i++){
+
+
+    list[i].addEventListener("click" , function(){
+
+      var current = document.getElementsByClassName("active");
+
+      current[0].className = current[0].className.replace(" active" , "");
+
+      this.className += " active";
+
+
+    })
+
+
+  }
+
+
+
+}
+
+
+
+
+
+
+
+
+function abc(){
+
+
+  active()
+
+
+  let htmlCode = ""
+
+  carcolors.forEach((car) =>{
+
+
+    var tabcontent = document.getElementById('mylg-tabcontent')
+    var news = document.getElementById("1")
+    var events = document.getElementById("2")
+
+    
+    
+    if(car.color === 'red' && news === document.getElementById("1")){
+      
+      
+      htmlCode += `
+      
+      
+      <div class="datalist"> 
+      
+
+        <div style="display : flex ; flex-direction : row; ">
+
+          <div style=" width : 8%" class = "proli">
+
+            <div class="calender">
+
+              MAR
+
+            </div>
+
+            <div class="calender1">
+
+              21
+
+            </div>
+          
+
+          </div>
+
+          <div class = "descDiv">
+      
+            ${car.capacity} 
+            ${car.type} 
+            ${car.color} 
+            ${car.registration}
+      
+          </div>
+
+          <div style=" width : 8%">
+
+            <div>
+
+              <a class="btn btn-sucess btn-sm" target="_blank" title="View"><i class="fa fa-eye"></i></a>
+
+            </div>
+
+
+          </div>
+    
+        
+        </div>
+        
+        
+      </div>`
+      
+      
+      tabcontent.innerHTML = htmlCode
+
+      
+      
+    }
+
+   
+  })
+
+
+}
+
+
+function bcd(){
+
+  active()
+
+
+
+  let htmlCode = ""
+
+  carcolors.forEach((car) =>{
+
+
+    var tabcontent = document.getElementById('mylg-tabcontent')
+    var news = document.getElementById("1")
+    var events = document.getElementById("2")
+
+    
+    
+    if(car.color === 'purple' ){
+      
+      
+      htmlCode += `
+      
+      
+      <div class="datalist"> 
+      
+
+        <div style="display : flex ; flex-direction : row; ">
+
+          <div style=" width : 4%" class = "proli">
+
+            <div class="calender">
+
+              MAR
+
+            </div>
+
+            <div class="calender1">
+
+              21
+
+            </div>
+          
+
+          </div>
+
+          <div class = "descDiv">
+      
+            ${car.capacity} 
+            ${car.type} 
+            ${car.color} 
+            ${car.registration}
+
+            <div>
+
+              <a class= "btn icon-btn btn-primary">
+              
+                <span class="btn-update fa fa-link img-circle text-muted">
+                
+                  Notification
+
+                </span>
+              
+              </a>
+              <a class= "btn icon-btn btn-primary">
+              
+                <span class="btn-update fa fa-link img-circle">
+                
+                  Notification
+
+                </span>
+              
+              </a>
+
+            </div>
+
+      
+          </div>
+
+          <div style=" width : 8%">
+
+            <div>
+
+              <a class="btn btn-sucess btn-sm" target="_blank" title="View"><i class="fa fa-eye"></i></a>
+
+            </div>
+
+
+          </div>
+    
+        
+        </div>
+        
+        
+      </div>`
+      
+      
+      
+      tabcontent.innerHTML = htmlCode
+      
+    }
+
+
+
+  })
+
+}
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("mylg-tabcontent").style.top = "0";
+  } else {
+    document.getElementById("mylg-tabcontent").style.top = "-50px";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+// function abc(evt , taba) {
+
+
+//     evt.currentTarget.className += " active";
+
+
+
+
+
+
+
+
+//     var tab = tabs.filter((tabs) => tabs["Cat"] === 1)
+//     // console.log(tab)
+
+//     displaylist()
+
+
+// }
+
+
+
+
